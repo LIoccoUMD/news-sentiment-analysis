9ede975fcbac80cbe26b8d28e8e03527c111a6c1
LIoccoUMD <liocco@umd.edu>
2025-02-28 13:03:04 -0500

    Added MSNBC. cleaned print

--- nlp.py
+++ nlp.py
@@ -10,12 +10,13 @@ from transformers import pipeline
 import torch
 from datasets import Dataset # Models perform better on datasets
 
-
+# News stations. Should probably make this a list or maybe even dictionary?
+     # newsStations = {BBC1:"...news-1", BBC2:"...news2" BBC3:"...news3"}
 cnn = "https://www.cnn.com/sitemap/news.xml"
 fox = "https://www.foxnews.com/sitemap.xml?type=news"
 bbc = "https://www.bbc.com/sitemaps/https-sitemap-com-news-2.xml" # BBC has 3 news urls (news-1, news-2, news-3).
 reuters = "https://www.reuters.com/arc/outboundfeeds/news-sitemap/?outputType=xml"
-
+msnbc = "https://www.msnbc.com/sitemap/msnbc/sitemap-news"
 
 def scrapeTitlesXML(url):
     response = requests.get(url)
@@ -32,7 +33,7 @@ def titleSentimentAnalysis(titles):
     headlines = [title.text for title in titles]
     dataset = Dataset.from_dict({"articleTitles": headlines})
     classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
-    if torch.cuda.is_available(): # Stop this from printing
+    if torch.cuda.is_available(): # TO-DO: Stop this from printing
         classifier.model.to("cuda")
     
     results = classifier(dataset["articleTitles"], batch_size=32)
@@ -51,14 +52,16 @@ def printAllTitles(titles, scores):
 def printPosandNeg(scores, station):
     numPositive = sum(1 for result in scores if result["label"] == "POSITIVE")
     numNegative = sum(1 for result in scores if result["label"] == "NEGATIVE")
-    print(f"{station} Count of POSITIVE scores: {numPositive}")
-    print(f"{station} Count of NEGATIVE scores: {numNegative}")
+    print(f"{station} number of POSITIVE article titles: {numPositive}")
+    print(f"{station} number of NEGATIVE article titles: {numNegative}\n")
 
 def main():
     print(f"CUDA availability: {torch.cuda.is_available()}")
+    
     # CNN news
     cnnTitles = scrapeTitlesXML(cnn)
     cnnScores = titleSentimentAnalysis(cnnTitles)
+    
     # Fox news
     foxTitles = scrapeTitlesXML(fox)
     foxScores = titleSentimentAnalysis(foxTitles)
@@ -67,13 +70,18 @@ def main():
     bbcTitles = scrapeTitlesXML(bbc)
     bbcScores = titleSentimentAnalysis(bbcTitles)
 
-    #Reuters
+    # Reuters
     reutersTitles = scrapeTitlesXML(reuters)
     reutersScores = titleSentimentAnalysis(reutersTitles)
+
+    # MSNBC
+    msnbcTitles = scrapeTitlesXML(msnbc)
+    msnbcScores = titleSentimentAnalysis(msnbcTitles)
 
     printPosandNeg(cnnScores, "CNN")
     printPosandNeg(foxScores, "FOX")
     printPosandNeg(bbcScores, "BBC")
     printPosandNeg(reutersScores, "Reuters")
+    printPosandNeg(msnbcScores, "MSNBC")
 
 main()
