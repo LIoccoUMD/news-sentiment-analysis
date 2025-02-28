6bec1832cbcb6a7cc021f3ca902eb27e10944f1a
LIoccoUMD <liocco@umd.edu>
2025-02-27 19:13:01 -0500

    Initial Commit. Completed sentiment analysis on FOX news and CNN news article headlines

---
+++ nlp.py
@@ -0,0 +1,65 @@
+import nltk
+# nltk.download('punkt_tab')
+# import spaCy
+import numpy
+import requests
+from bs4 import BeautifulSoup
+import time
+import lxml # BeautifulSoup uses this to parse xml
+from transformers import pipeline
+import torch
+from datasets import Dataset # Models perform better on datasets
+
+cnn = "https://www.cnn.com/sitemap/news.xml"
+fox = "https://www.foxnews.com/sitemap.xml?type=news"
+# Scrape the url for
+def scrapeTitlesXML(url):
+    response = requests.get(url)
+    # Store the xml 
+    content = response.text
+    # Soupify!
+    soup = BeautifulSoup(content, "xml")
+    # Tokenize the title of every article
+    titles = soup.find_all("news:title") # 297 article titles total
+    return titles
+
+
+# Load the sentiment analysis pipeline
+def titleSentimentAnalysis(titles):
+    headlines = [title.text for title in titles]
+    dataset = Dataset.from_dict({"articleTitles": headlines})
+    classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+    if torch.cuda.is_available():
+        classifier.model.to("cuda")
+    
+    results = classifier(dataset["articleTitles"], batch_size=32)
+    print(f"CUDA availability: {torch.cuda.is_available()}")
+
+    return results
+
+# Prints every title along with its lable (POSTIIVE OR NEGATIVE) and it's score (-1 to 1)
+def printAllTitles(titles, scores):
+    for i, (title, result) in enumerate(zip(titles, scores)):
+        print(f"Title {i+1}: {title.text}")
+        print(f"  Label: {result['label']}")
+        print(f"  Score: {result['score']}\n")
+
+
+# Prints the number of article titles with positive and negative sentiment analysis.
+def printPosandNeg(scores, station):
+    numPositive = sum(1 for result in scores if result["label"] == "POSITIVE")
+    numNegative = sum(1 for result in scores if result["label"] == "NEGATIVE")
+    print(f"{station} Count of POSITIVE scores: {numPositive}")
+    print(f"{station} Count of NEGATIVE scores: {numNegative}")
+
+def main():
+    # CNN news
+    cnnTitles = scrapeTitlesXML(cnn)
+    cnnScores = titleSentimentAnalysis(cnnTitles)
+    # Fox news
+    foxTitles = scrapeTitlesXML(fox)
+    foxScores = titleSentimentAnalysis(foxTitles)
+    printPosandNeg(cnnScores, "CNN")
+    printPosandNeg(foxScores, "FOX")
+
+main()
