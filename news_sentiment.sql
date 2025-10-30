c254fb2da44ad3c628015ca80eb903691ea06a49
LIoccoUMD <liocco@umd.edu>
2025-10-30 12:50:33 -0400

    updated dependencies

--- news_sentiment.sql
+++ news_sentiment.sql
@@ -4,16 +4,16 @@
 -- FLUSH PRIVILEGES;
 
 
--- CREATE DATABASE news_sentiment;
--- USE news_sentiment;
--- CREATE TABLE articles (
--- id INT AUTO_INCREMENT PRIMARY KEY,
--- title VARCHAR(255),
--- source VARCHAR(50),
--- sentiment_label VARCHAR(10),
--- sentiment_score FLOAT,
--- scraped_date DATE
--- );
--- SHOW TABLES;
+CREATE DATABASE news_sentiment;
+USE news_sentiment;
+CREATE TABLE articles (
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(255),
+source VARCHAR(50),
+sentiment_label VARCHAR(10),
+sentiment_score FLOAT,
+scraped_date DATE
+);
+SHOW TABLES;
 
 SELECT * FROM articles
